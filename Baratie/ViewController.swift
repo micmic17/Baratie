1a9641a5c8a2adafffa817804000641abde2f7e6
micmic17 <mickale_saturre@ligph.com>
2021-03-26 09:38:43 +0800

    Initial Commit

---
+++ Baratie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baratie
+//
+//  Created by Mickale Saturre on 3/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
