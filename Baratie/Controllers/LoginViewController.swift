b6903b6a6fe5a2a461415530179905831ebbde5d
micmic17 <mickale_saturre@ligph.com>
2021-03-29 11:57:36 +0800

    Added Login Initial Design

--- Baratie/Controllers/LoginViewController.swift
+++ Baratie/Controllers/LoginViewController.swift
@@ -7,10 +7,36 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-}
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
 
+        customView(imageView, "Login")
+        customTextField(emailTextField, "Email")
+        customTextField(passwordTextField, "Password")
+        customButton(loginButton)
+    }
+    
+    @IBAction func loginTextFieldChanges(_ sender: UITextField) {
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
