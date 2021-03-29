e9630d33c07ec2aff0f76ebfe55ab1a7b73aff7e
micmic17 <mickale_saturre@ligph.com>
2021-03-29 13:16:26 +0800

    Added Register Initial Design

--- Baratie/Controllers/RegisterViewController.swift
+++ Baratie/Controllers/RegisterViewController.swift
@@ -8,8 +8,29 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        customView(imageView, "Register")
+        customTextField(firstNameTextField, "Firstname")
+        customTextField(lastNameTextField, "Lastname")
+        customTextField(emailTextField, "Email")
+        customTextField(addressTextField, "Address")
+        customTextField(passwordTextField, "Password")
+        customTextField(confirmPasswordTextField, "Confirm Password")
+        customButton(registerButton)
+    }
+
+    @IBAction func formTextFieldChanged(_ sender: Any) {
+    }
+    @IBAction func registerButtonPressed(_ sender: Any) {
     }
 }
